6fc61c5aff79f51e8c07d4329b7b656afda675cf
DIMSITS <sethdemeterio47@gmail.com>
2024-12-01 18:26:22 +0800

    feat: routes for register and login added

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express =  require('express');
+const { registerUser, loginUser } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Public Routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);  
+
+// Protected Routes
+router.get('/profile', authMiddleware, (req, res) => {
+    res.json({ message: 'Protected Route' });
+});
+
+module.exports = router;
