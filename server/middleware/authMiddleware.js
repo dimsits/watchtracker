a68f17d45fa3b232cd950ac09e5a9a7b3ba636a6
DIMSITS <sethdemeterio47@gmail.com>
2024-12-01 18:23:33 +0800

    feat: token authentication added

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function authMiddleware(req, res, next) {
+    const token = req.header.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+        const user = jwt.verify(token, JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid Token' });
+    }
+}
+
+module.exports = authMiddleware;
